ae4daebe73182537d1ecf70f28d7e7737f6e7793
Felpix <jshiansongli@gmail.com>
2023-03-17 13:24:43 -0400

    mouse scaling

--- fancypointer/script.js
+++ fancypointer/script.js
@@ -3,6 +3,16 @@ const cursorInner = document.querySelector('.cursor-inner');
 
 //move the cursorOuter to be on the mouse position
 document.addEventListener('mousemove', e => {
-  cursorOuter.style.top = e.pageY + 'px';
-  cursorOuter.style.left = e.pageX + 'px';
+  cursorOuter.style.top = e.pageY-8 + 'px';
+  cursorOuter.style.left = e.pageX-8 + 'px';
+});
+
+//on mouse pressed down
+document.addEventListener('mousedown', () => {
+    cursorOuter.style.transform = 'scale(1.5)';
+});
+
+//on mouse released
+document.addEventListener('mouseup', () => {
+    cursorOuter.style.transform = 'scale(1)';
 });
